e9e8d84727d3ee818fbdc8bc073919ffcd83c0eb
priscila.moro <priscila.moro@rappi.com>
2023-01-05 16:10:13 -0300

    pr 1 - Unowned Optional Reference

--- MyPlayground.playground/Pages/Unowned Optional Reference.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Unowned Optional Reference.xcplaygroundpage/Contents.swift
@@ -7,6 +7,9 @@ class Department {
         self.name = name
         self.courses = []
     }
+    deinit {
+        print("killed")
+    }
 }
 
 class Course {
@@ -34,3 +37,11 @@ department!.courses = [intro, intermediate, advanced]
 department = nil
 // What will be printed?
 print(intermediate.department)
+// print: Error
+// La instancia department se crea de tipo Department?, por lo que puede llegar a ser nil.
+// El problema es que la propiedad department de los courses no es del tipo opcional, es una unowned reference,
+// por lo que swift no espera que la propiedad department de los courses pueda llegar a ser nil.
+// Por otro lado, las unowned reference necesitan hacer referencia a clases que van a sobrevivir a largo plazo, en este sentido no puede
+// existir un course sin department, pero si un department sin courses, porque Course tiene una unowned reference con Department,
+// entonces cuando la instancia department se desasigna, no hay mas referencia fuerte a Department,
+// por lo que se pierde la referencia con Course
